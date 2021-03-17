d47c118536c7ed185a015eb8a0575e14e269059b
Kerim ALTUN <76987626+zigetvar@users.noreply.github.com>
2021-03-17 05:18:35 +0300

    C#'la Apartman Takip Sistemi

---
+++ ApartmanTakipSistemi/Personİnformation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApartmanTakipSistemi
+{
+    static class Personİnformation
+    {
+        public static void İnformation()
+        {
+            Console.Write("Kat:");
+            string floorNum = Console.ReadLine();
+            Console.Write("İsim:");
+            string name = Console.ReadLine();
+            Console.Write("Soyisim:");
+            string surname = Console.ReadLine();
+            Console.Write("Yaş:");
+            string age = Console.ReadLine();
+
+        }
+
+        public static void ReceivedMoney()
+        {
+            Console.Write("Alınan para:");
+            string received_money = Console.ReadLine();
+        }
+
+        public static void yapilanİslemler()
+        {
+            Console.Write("Yapılan işlemler:");
+            string yapilan_islemler = Console.ReadLine();
+        }
+    }
+}
---
+++ ApartmanTakipSistemi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ApartmanTakipSistemi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Personİnformation.İnformation();
+                Personİnformation.ReceivedMoney();
+                Personİnformation.yapilanİslemler();
+                Console.WriteLine("*********************");
+                Console.WriteLine("Kayıt İşlemi Bitti Mi? : /e/h ");
+                string kontrol = Console.ReadLine();
+                if (kontrol=="e")
+                {
+                    Console.WriteLine("Kayıt İşlemi Tamamlandı...");
+                    break;
+                   
+                }
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
